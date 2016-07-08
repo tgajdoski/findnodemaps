f394be7ebe2f11cb9b0ef9275efe7d5211b06c2e
Gajdo <t.gajdoski@gmail.com>
2016-07-08 15:10:33 +0200

    final touch

--- public/js/twitterStream.js
+++ public/js/twitterStream.js
@@ -373,30 +373,15 @@ $(document).ready(function() {
 
 		$( "#popupmarker" ).click(function() {
 			
-			$('#modal-form').modal('show');
+			
 			
 			$('#latit').val("");
-		 $('#longit').val("");
-		 $('#markertext').val("");
-					/* 	 var myLatlng = new google.maps.LatLng(17.7850,-12.4183);
-						 
-						  var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
-  
-  
-  
-						var myOptions = {
-						zoom: 2,
-						center: myLatlng,
-						mapTypeId: google.maps.MapTypeId.ROADMAP,
-						mapTypeControl: true,
-						mapTypeControlOptions: {
-						  style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-						  position: google.maps.ControlPosition.LEFT_BOTTOM
-						},
-						styles: light_grey_style
-					  };
-						
-					var map = new google.maps.Map($('#map_canvas_click')[0], myOptions); */	
+			$('#longit').val("");
+		 
+			$('#markertext').val("");
+			$('#modal-form').modal('show');
+		 
+					
 					
 					
  var myLatlng = new google.maps.LatLng(33.84981264529933,-84.29855352267623);
@@ -404,7 +389,7 @@ $(document).ready(function() {
      	
 						var map=document.getElementById("map_canvas_click");
 		var settings = {
-        zoom: 11,
+        zoom: 8,
         center: myLatlng,
         scaleControl: true,
         mapTypeControlOptions: {
